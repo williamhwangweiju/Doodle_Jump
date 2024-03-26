9aa10fbf1ddd06f94fd5bf933ccbd27edaa4d87e
williamhwangweiju <114830853+williamhwangweiju@users.noreply.github.com>
2024-03-26 09:37:04 -0400

    Implemented draw_platform() and Platform_draw()

--- doodle_jump.c
+++ doodle_jump.c
@@ -97,7 +97,7 @@ void Player_draw(struct Player* player) {
 	// WILLIAM
 
 	int image_width = 41; // Image width based on doodle_map dimensions
-    int image_height = 40; // Image height based on doodle_map dimensions
+	int image_height = 40; // Image height based on doodle_map dimensions
 
 	int start_x = (int)(player->pos.x);
     int start_y = (int)(player->pos.y);
@@ -142,6 +142,30 @@ void Platform_draw(struct Platform* platform);
 
 void Platform_draw(struct Platform* platform) {
 	// WILLIAM
+
+	int image_width = 60; // Image width based on platform_map dimensions
+	int image_height = 16; // Image height based on platform_map dimensions
+
+	int start_x = (int)(platform->pos.x);
+    int start_y = (int)(platform->pos.y);
+	
+	draw_platform();
+
+	// Draw the doodle image
+    for (int y = 0; y < image_height - 1; y++) {
+        for (int x = 0; x < image_width; x++) {
+            uint16_t pixel_color = platform_map_2d[y][x];
+            if (pixel_color != 0) {
+            plot_pixel(start_x + x, start_y + y, pixel_color);
+			}
+        }
+    }
+
+    // Free memory allocated for 2D array
+    for (int i = 0; i < image_height; i++) {
+        free(platform_map_2d[i]);
+    }
+    free(platform_map_2d);
 }
 
 /*********************************
@@ -250,15 +274,15 @@ void Game_draw(struct Game* game) {
 void Game_drawBackground() {
 	// WILLIAM
 
-	int image_width = 320; // Image width based on doodle_map dimensions
-    int image_height = 247; // Image height based on doodle_map dimensions
+	int image_width = 320; // Image width based on background_map dimensions
+    int image_height = 247; // Image height based on background_map dimensions
 
 	int start_x = (320 - image_width) / 2; // Center the image horizontally
     int start_y = (240 - image_height) / 2; // Center the image vertically
 
 	draw_background();
 
-	// Draw the doodle image
+	// Draw the background image
     for (int y = 0; y < image_height - 1; y++) {
         for (int x = 0; x < image_width; x++) {
             uint16_t pixel_color = background_map_2d[y][x];
