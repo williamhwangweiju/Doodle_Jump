4b3f836f67286bcc30c80aeb73dd2f2cace595e7
williamhwangweiju <114830853+williamhwangweiju@users.noreply.github.com>
2024-04-05 18:06:12 -0400

    HEX display

--- doodle_jump.c
+++ doodle_jump.c
@@ -31,6 +31,20 @@ struct Platform
 short int Buffer1[240][512];
 short int Buffer2[240][512];
 
+// Seven-segment display segments (a - g)
+const uint8_t segments[10] = {
+    0b00111111, // 0
+    0b00000110, // 1
+    0b01011011, // 2
+    0b01001111, // 3
+    0b01100110, // 4
+    0b01101101, // 5
+    0b01111101, // 6
+    0b00000111, // 7
+    0b01111111, // 8
+    0b01101111  // 9
+};
+
 uint16_t** platform_map_2d;
 uint16_t** platform2_map_2d;
 uint16_t** platform3_map_2d;
@@ -74,6 +88,9 @@ const int platform3Height = 15;
 
 // PS2 input
 volatile int* PS2_ptr = (int*)0xFF200100;
+
+volatile int* HEX_BASE1 = (int*)0xFF200020;
+volatile int* HEX_BASE2 = (int*)0xFF200030;
 
 volatile unsigned int key1, key2;
 bool rightArrow = false;
@@ -1036,6 +1053,7 @@ void setup_gameOver();
 struct Vector world_to_screen(struct Vector pos, int bottom);
 void checkKey(bool* keyBool, unsigned char code);
 void updateKeys();
+void displayScore(int score);
 
 int main()
 {
@@ -1122,6 +1140,9 @@ int main()
 
 		struct Vector monster1Pos = { -300, -300 };
 		struct Vector monster2Pos = { -300, -300 };
+		
+		// Set/reset the LEDs
+		displayScore(0);
 
 		// Stay in the menu until pressed space
 		while (space == false)
@@ -1192,6 +1213,8 @@ int main()
 			// Update saved bottomOfScreen
 			secondPrevBottomOfScreen = prevBottomOfScreen;
 			prevBottomOfScreen = bottomOfScreen;
+
+			displayScore(bottomOfScreen);
 
 			// Determine if monster should maybe be spawned
 			if (bottomOfScreen > numMilestones * milestoneHeight)
@@ -2102,4 +2125,19 @@ void updateKeys()
 	checkKey(&leftArrow, 0x6B);
 	checkKey(&rightArrow, 0x74);
 	checkKey(&space, 0x29);
+}
+
+void displayScore(int score) {
+    // Array to store individual digits of the score
+    int digits[6] = {0};
+
+    // Extract digits from the score
+    for (int i = 0; i < 6; ++i) {
+        digits[i] = score % 10; // Extract least significant digit
+        score /= 10; // Remove least significant digit
+    }
+
+    // Display each digit on the seven-segment displays
+    *HEX_BASE1 = (segments[digits[3]] << 24) | (segments[digits[2]] << 16) | (segments[digits[1]] << 8) | (segments[digits[0]]);
+	*HEX_BASE2 = (segments[digits[5]] << 8) | (segments[digits[4]]);
 }
